075833f31f564a229af26ec88b42fdf0143c1032
TheDarkKnight04 <santam040802@gmail.com>
2025-01-26 14:02:36 -0800

    Completed Bonus : Ball can jump now

--- Assets/Scenes/BallController.cs
+++ Assets/Scenes/BallController.cs
@@ -6,10 +6,30 @@ public class BallController : MonoBehaviour
     [SerializeField] private Rigidbody sphereRigidbody;
 
     [SerializeField] private float ballSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void MoveBall(Vector2 input)
+
+    [SerializeField] private float jumpHeight;
+
+    private bool isGrounded = false; // To prevent double jumps
+
+    private void OnCollisionEnter(Collision collision)
     {
-        Vector3 inputXZPlane = new(input.x, 0, input.y);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        } 
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void MoveBall(Vector3 input)
+    {
+        Vector3 force = new Vector3(input.x, 0, input.z) * ballSpeed;
+        sphereRigidbody.AddForce(force, ForceMode.Force);
+
+        if(input.y > 0 && isGrounded)
+        {
+            Vector3 jumpForceVector = Vector3.up * jumpHeight;
+
+            sphereRigidbody.AddForce(jumpForceVector, ForceMode.Impulse);
+            isGrounded=false;
+        }
     }
 }
